b538686d4d165d50655f2ac902e6a5c4e96cea7b
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-01-02 22:56:56 +0000

    New cleardb connection

--- includes/database.php
+++ includes/database.php
@@ -1,13 +1,19 @@
 <?php
 
-    $servername = getenv('IP');
-    $username = "badgerlooplogist";
-    $password = "badgers28";
-    $database = "badgerloop_db";
-    $dbport = 3306;
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"], 1);
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $database, $dbport);
+    $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
+    $db['default']['hostname'] = $cleardb_server
+    $db['default']['username'] = $cleardb_username
+    $db['default']['password'] = $cleardb_password
+    $db['default']['database'] = $cleardb_db
 
     // Check connection
     if ($db->connect_error) {
