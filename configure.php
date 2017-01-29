46710aa105a891c240366ee4da043fc1686e203c
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-01-29 03:11:38 +0000

    testing

--- configure.php
+++ configure.php
@@ -7,7 +7,7 @@ use ThreadMeUp\Slack\Client;
 $config = [
     'token' => 'xoxp-133915446628-133979134660-133980674036-b066a8fdbd397e915ae15f5009a266f5',
     'team' => 'new',
-    'username' => 'dillongwozdz',
+    'username' => 'gwozdz',
     'icon' => 'ICON', // Auto detects if it's an icon_url or icon_emoji
     'parse' => '', // __construct function in Client.php calls for the parse parameter 
 ];
