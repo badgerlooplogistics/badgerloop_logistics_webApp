4f01d87c2df539c2d99c9c805ab8284f6b7a1557
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-01-30 02:53:31 +0000

    testing

--- includes/orderrequestlist.php
+++ includes/orderrequestlist.php
@@ -45,7 +45,7 @@
                         echo "<tr id='requestListTr".$request['id']."'>
                                 <td>".$request['item']."</td>
                                 <td>".date_format(date_create($request['date_added']), 'm/d/Y')."</td>
-                                <td>".$request['user_name']."</td>
+                                <td>".$request['user_id']."</td>
                                 <td>".$priority."</td>
                                 <td style='text-align: center;'><button id='".$request['id']."' class='btn requestListButton requestListButtonApprove'><span class='glyphicon glyphicon-ok'></span></button> <button id='".$request['id']."' class='btn requestListButton requestListButtonReject'><span class='glyphicon glyphicon-remove'></span></button></td>
                                 <td style='text-align: center;'><button class=\"btn requestListButton\" data-toggle=\"modal\" data-target=\"#myModal".$request['id']."\"><span class='glyphicon glyphicon-search'></span></button></td>
@@ -60,7 +60,7 @@
                                             </div>
                                             <div class=\"modal-body\">
                                                 <p style='color:black;'>
-                                                    Requested By: ".$request['user_name']."<br />
+                                                    Requested By: ".$request['user_id']."<br />
                                                     Item: ".$request['item']."<br />
                                                     System: ".$request['system']."<br />
                                                     Item Description: ".$request['item_disc']."<br />
