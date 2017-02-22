3da9811741226b23111d5dbad2d222bd06470f53
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-22 19:02:11 +0000

    Working on notification system

--- includes/ajax/requestListApprove.php
+++ includes/ajax/requestListApprove.php
@@ -4,9 +4,10 @@ require_once("../database.php");
 
 $id = $_POST['id'];
 
-//$cmd = "curl -X POST --data-urlencode 'payload'={"'channel": "@kkfisher3", "username": "webhookbot", "text": "This is posted to #purchasing and comes from a bot named webhookbot.", "icon_emoji": ":ghost:"}' 'https://hooks.slack.com/services/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"';
-//exec($cmd,$result);
 
 $query = "UPDATE financial_package SET status = 1 WHERE id = ".$id;
 mysqli_query($conn, $query);
+
+$cmd = "curl -X POST --data-urlencode payload={'channel': '@kkfisher3', 'username': 'webhookbot', 'text': 'This is comes from a bot named George.', 'icon_emoji': ':ghost:'} 'https://hooks.slack.com/services/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'";
+exec($cmd,$result);
 ?>
