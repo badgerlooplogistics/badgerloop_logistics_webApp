79e69c7eefee229615b3bc4c5f5a8ecada46c8ca
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-22 17:13:29 +0000

    Attempting to Fix BOM button

--- includes/ajax/addBOM.php
+++ includes/ajax/addBOM.php
@@ -2,7 +2,7 @@
 
 require_once("../database.php");
 session_start();
-
+/*
 $supplier = mysqli_real_escape_string($conn, $_POST['supplier']);
 $cost = mysqli_real_escape_string($conn, $_POST['cost']);
 $quantity = mysqli_real_escape_string($conn, $_POST['quantity']);
@@ -12,6 +12,7 @@ $comments = mysqli_real_escape_string($conn, $_POST['comments']);
 $date = date('Y-m-d', time());
 $id = $_POST['id'];
 $userId = $_SESSION['user'];
+*\
 /*
 if ($cost == '') {
     $cost = 0;
@@ -27,10 +28,19 @@ if ($tax == '') {
 }
 */
 
-$query = "UPDATE financial_package SET act_supplier = '{$supplier}', act_individual_cost = {$cost}, act_quantity = {$quantity}, shipping = {$shipping}, tax = {$tax}, date_purchased = '{$date}', purchased_by = {$userId}, comments = '{$comments}', status = 3 WHERE id = {$id}";
-mysqli_query($conn, $query);
-// update spent_total
+$id = $_POST['id'];
 
+
+$query = "UPDATE financial_package SET status = 3 WHERE id = ".$id;
+mysqli_query($conn, $query);
+
+
+
+//$query = "UPDATE financial_package SET act_supplier = '{$supplier}', act_individual_cost = {$cost}, act_quantity = {$quantity}, shipping = {$shipping}, tax = {$tax}, date_purchased = '{$date}', purchased_by = {$userId}, comments = '{$comments}', status = 3 WHERE id = {$id}";
+//mysqli_query($conn, $query);
+
+// update spent_total
+/*
 $query = "SELECT user_id FROM financial_package WHERE id={$id}";
 $result = mysqli_query($conn, $query);
 $userIdArray = mysqli_fetch_assoc($result);
@@ -46,14 +56,14 @@ $query = "SELECT spent_total FROM teams WHERE id = {$teamId}";
 $result = mysqli_query($conn, $query);
 $team = mysqli_fetch_assoc($result);
 $spentTotal = $team['spent_total'];
-
+*\
 
 // calculate total cost
-$totalCost = $cost*$quantity + $shipping + $tax;
-$newAmount = $spentTotal + $totalCost;
+//$totalCost = $cost*$quantity + $shipping + $tax;
+//$newAmount = $spentTotal + $totalCost;
 
-$query = "UPDATE teams SET spent_total = {$newAmount} WHERE id={$teamId}";
-mysqli_query($conn, $query);
+//$query = "UPDATE teams SET spent_total = {$newAmount} WHERE id={$teamId}";
+//mysqli_query($conn, $query);
 
 
 ?>
