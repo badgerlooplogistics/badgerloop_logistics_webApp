37995dd7c893b8a6492a41b4c7431b2d841a1a5a
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-23 06:47:47 +0000

    Testing slack bot

--- includes/ajax/request.php
+++ includes/ajax/request.php
@@ -41,7 +41,7 @@ if (mysqli_affected_rows($conn) > 0) {
 $purchasers_name = "Elon Musk";
 $amount = "500 dollars";
 
-$message = "Hello! There is a new purchase request from ".$purchasers_name." for ".$amount; 
+$message = "Hello! There is a new purchase request from ".$purchasers_name." for ".$cost; 
 $room = "kkfisher3"; 
 $icon = ":moyai:"; 
 $username = "Georgebot";
