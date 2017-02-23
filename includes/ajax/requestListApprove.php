80dee1e851adb475e665ab17c57403ca7744e9e0
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-23 05:57:47 +0000

    Testing slack bot

--- includes/ajax/requestListApprove.php
+++ includes/ajax/requestListApprove.php
@@ -4,12 +4,13 @@ require_once("../database.php");
 
 $id = $_POST['id'];
 
-$cmd = "curl -X POST --data-urlencode 'payload={'channel': '@kkfisher3', 'username': 'webhookbot', 'text': 'This is comes from a bot named George.', 'icon_emoji': ':ghost:'}' https://hooks.slack.com/services/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";
-//$result = mysqli_query($conn, $query);
-exec($cmd);
 
 $query = "UPDATE financial_package SET status = 1 WHERE id = ".$id;
 mysqli_query($conn, $query);
 
+$cmd = "curl -X POST --data-urlencode 'payload={'channel': '@kkfisher3', 'username': 'webhookbot', 'text': 'This is comes from a bot named George.', 'icon_emoji': ':ghost:'}' https://hooks.slack.com/services/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";
+$result = mysqli_query($conn, $query);
+exec($cmd,$result);
 
 ?>
+
