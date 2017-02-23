9b05ea75ba427177ed90c184a4cfd77b52d96d8f
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-23 06:06:17 +0000

    Testing slack bot

--- includes/ajax/requestListApprove.php
+++ includes/ajax/requestListApprove.php
@@ -8,9 +8,36 @@ $id = $_POST['id'];
 $query = "UPDATE financial_package SET status = 1 WHERE id = ".$id;
 mysqli_query($conn, $query);
 
-$cmd = "curl -X POST --data-urlencode 'payload={'channel': '@kkfisher3', 'username': 'webhookbot', 'text': 'This is comes from a bot named George.', 'icon_emoji': ':ghost:'}' https://hooks.slack.com/services/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";
-$result = mysqli_query($conn, $query);
-exec($cmd,$result);
+//$cmd = "curl -X POST --data-urlencode 'payload={'channel': '@kkfisher3', 'username': 'webhookbot', 'text': 'This is comes from a bot named George.', 'icon_emoji': ':ghost:'}' https://hooks.slack.com/services/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";
+//$result = mysqli_query($conn, $query);
+//exec($cmd,$result);
+
+
+$message = "hello world from curl"; 
+$room = "kkfisher3"; 
+$icon = ":smile:"; 
+$data = "payload=" . json_encode(array(         
+        "channel"       =>  "@{$room}",
+        "text"          =>  $message,
+        "icon_emoji"    =>  $icon
+    ));
+$url = "https://hooks.slack.com/services/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";
+         
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+echo var_dump($result);
+if($result === false)
+{
+    echo 'Curl error: ' . curl_error($ch);
+}
+ 
+curl_close($ch);
 
 ?>
 
