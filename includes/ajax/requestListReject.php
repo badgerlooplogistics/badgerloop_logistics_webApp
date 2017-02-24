eafcc511af45c70bfc86fc688360c7c814fad144
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-24 22:38:29 +0000

    comments

--- includes/ajax/requestListReject.php
+++ includes/ajax/requestListReject.php
@@ -35,7 +35,13 @@ $value = "Top";
 //echo "<script>console.log('Top')</script>";
 //echo "<script>console.log($value)</script>";
 
-echo "<script>console.log\"(".$value."\")</scrip";
+echo "<script>console.log\"(".$value."\")</script>";
+echo "<script>console.log($value)</script>";
+echo "<script>console.log(($value))</script>";
+
+
+
+
 echo "<script>console.log($value)</script";
 echo("<script>Top</script>");
 echo("<script>$value</script>");
