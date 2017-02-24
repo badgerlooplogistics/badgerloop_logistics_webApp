7de8bab92a4d9dfb54580d5891400a3471c6c7e4
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-24 04:12:56 +0000

    fixing slack bot

--- includes/ajax/request.php
+++ includes/ajax/request.php
@@ -34,43 +34,9 @@ mysqli_query($conn, $query);
 
 if (mysqli_affected_rows($conn) > 0) {
     echo "success";
-    $purchasers_name = "Elon Musk";
-    $amount = "500 dollars";
-    
-    $message = "Hello! There is a new purchase request from ".$purchasers_name." for $".$cost; 
-    $room = "kkfisher3"; 
-    $icon = ":moyai:"; 
-    $username = "Georgebot";
-    $data = "payload=" . json_encode(array(         
-            "channel"       =>  "@{$room}",
-            "text"          =>  $message,
-            "icon_emoji"    =>  $icon,
-            "username"      =>  $username
-        ));
-    $url = "https://hooks.slack.com/services/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";
-             
-     
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    echo var_dump($result);
-    
-
-    if($result === false)
-    {
-        echo 'Curl error: ' . curl_error($ch);
-    }
- 
-    curl_close($ch);
-
-
 } else {
     echo "fail";
-}
+};
 
 /*
 $purchasers_name = "Elon Musk";
