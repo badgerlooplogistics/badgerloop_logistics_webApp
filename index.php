a202d17ca8a8bf9ed7dbab74294987aa0e3d9782
Badgerloop Logistics <badgerloop.logistics@gmail.com>
2017-02-26 02:42:56 +0000

    Fix Purchase Request -- Make them enter all fields!

--- index.php
+++ index.php
@@ -432,7 +432,7 @@
         var id = $("#approvedOrderId").val();
         
         //var dataString = "id=" + id //+ "&supplier=" + supplier + "&cost=" + cost + "&quantity=" + quantity + "&shipping=" + shipping + "&tax=" + tax + "&comments=" + comments;
-        var dataString = "Test"
+        var dataString = "Test";
         
         $.ajax({
             type: "POST",
@@ -440,8 +440,8 @@
             data: dataString,
             success: function(data) {
                 $('#myModal'+id).modal('hide');
-                //var tr = "approvedOrderTr" + id;
-                //$("#"+tr).hide("fade");
+                var tr = "approvedOrderTr" + id;
+                $("#"+tr).hide("fade");
                 console.log(data);
             },
             error: function(data) {
